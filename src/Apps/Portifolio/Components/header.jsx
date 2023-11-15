67f3b2bc7591c27c0ee7370982fd3f34e560c925
Tdroid <garotorocha90@gmail.com>
2023-11-15 02:11:18 -0300

    Project: tdroid.live first commit

--- src/Apps/Portifolio/Components/header.jsx
+++ src/Apps/Portifolio/Components/header.jsx
@@ -9,16 +9,16 @@ export const About_HeaderComponents = () => {
             <h1 className="about_header_title">{"<Tdroid />"}</h1>
             <li className="about_header_list">
                 <ul className="about_header_item">
-                    <a className="about_header_list_text">Sobre-mim</a>
+                    <a href="!#" className="about_header_list_text">Sobre-mim</a>
                     </ul>
                 <ul className="about_header_item">
-                    <a className="about_header_list_text">Experiência</a>
+                    <a href="!#" className="about_header_list_text">Experiência</a>
                     </ul>
                 <ul className="about_header_item">
-                    <a className="about_header_list_text">Certificações</a>
+                    <a href="!#" className="about_header_list_text">Certificações</a>
                     </ul>
                 <ul className="about_header_item">
-                    <a className="about_header_list_text">Contato</a>
+                    <a href="!#" className="about_header_list_text">Contato</a>
                     </ul>
 
                 <div className="about_header_divider"/>
