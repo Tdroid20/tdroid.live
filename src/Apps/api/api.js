557464149a0226e2fb5e655516cf112189ff4805
Tdroid <garotorocha90@gmail.com>
2023-12-07 14:01:52 -0300

    Feat: DotEnv Config

--- src/Apps/api/api.js
+++ src/Apps/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookie from "js-cookie";
 
-const apiURL = process.env.REACT_PUBLIC_APP_API ?? "aaaaa"
+const apiURL = process.env.REACT_APP_API ?? "aaaaa"
 
 export const API_SK = axios.create({
   baseURL: apiURL,
