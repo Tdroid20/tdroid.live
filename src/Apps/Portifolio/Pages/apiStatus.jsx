3181be993e47d774e4cc77308221054340c43f8c
Tdroid <garotorocha90@gmail.com>
2023-12-07 14:15:18 -0300

    Feat: DotEnv Config

--- src/Apps/Portifolio/Pages/apiStatus.jsx
+++ src/Apps/Portifolio/Pages/apiStatus.jsx
@@ -11,7 +11,9 @@ export const APIStatus = () => {
         try {
             const response = await API_SK.get("/");
             setDetails(response.data);
+            console.log(process.env.REACT_APP_API)
         } catch(error) {
+            console.log(process.env.REACT_APP_API)
             console.error("Error fetching API status:", error);
         }
     }
