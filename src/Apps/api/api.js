4203aabaf857a65e710c55ce85856f36b58c1436
Tdroid <garotorocha90@gmail.com>
2023-12-07 13:53:45 -0300

    Feat: Refactor API Status

--- src/Apps/api/api.js
+++ src/Apps/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookie from "js-cookie";
 
-const apiURL = process.env.REACT_APP_API ?? "https://6571c96bd61ba6fcc013910c.mockapi.io/api/v1/status"
+const apiURL = process.env.REACT_PUBLIC_APP_API ?? "aaaaa"
 
 export const API_SK = axios.create({
   baseURL: apiURL,
