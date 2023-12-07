efd7f9976ee79bf383e126e20a386b78fb73480c
JuanOPrevost <jucaprevost2@gmail.com>
2023-12-07 13:19:30 -0300

    Feat: Skills Implemented

--- src/Apps/Portifolio/Components/Footer/footer.jsx
+++ src/Apps/Portifolio/Components/Footer/footer.jsx
@@ -11,7 +11,7 @@ export const FooterComponent = () => {
                     <div className="row row-2">
                         <div className="col-sm-3 text-md-center"><h5><span></span><b><img src={SKFLogo} alt="Skyrus Productions" className="footer_SKFLogo" /></b></h5></div>
                         <div className="col-sm-3  my-sm-0 mt-5"><ul className="list-unstyled">
-                            <li className="mt-0"><i className="fa-solid fa-server"></i> API</li>
+                            <li className="mt-0"><a href="/api/status"><i className="fa-solid fa-server"></i> API</a></li>
                             <li><i className="fa-regular fa-gem"></i> Beta Presence</li>
                             <li><i className="fa-solid fa-clock-rotate-left"></i> ChangeLogs</li>
                         </ul>
